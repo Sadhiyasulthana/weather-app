b74b577a4c03e124a540294b37fee5c51878e1fa
N M Sadhiya Sulthana <nmsadhiyasulthana31@gmail.com>
2026-03-12 12:47:21 +0530

    Add files via upload
    A simple weather application that shows real-time weather data for any city

---
+++ script.js
@@ -0,0 +1,31 @@
+function getWeather(){
+
+let city = document.getElementById("city").value;
+
+let apiKey = "df17b375a34cb68094bb956b0089abbb";
+
+let url = "https://api.openweathermap.org/data/2.5/weather?q=" 
++ city + 
+"&appid=" + apiKey + 
+"&units=metric";
+
+fetch(url)
+.then(function(response){
+return response.json();
+})
+.then(function(data){
+
+if(data.cod == "404"){
+alert("City not found");
+return;
+}
+
+document.getElementById("temp").innerHTML =
+"Temperature: " + data.main.temp + "°C";
+
+document.getElementById("weather").innerHTML =
+"Weather: " + data.weather[0].main;
+
+});
+
+}
